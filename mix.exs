205c7487f5eebc5438358cc913837115ce202306
Tobias Casper <tobias.casper@gmail.com>
2020-08-24 21:59:20 +0200

    chore: bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ESClient.MixProject do
   def project do
     [
       app: :es_client,
-      version: "0.2.0-dev",
+      version: "1.0.0",
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
