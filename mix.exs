cca844405e5c6f9b455a47f8e16550dc83f01e12
Kilian Gärtner <kilian.gaertner@i22.de>
2025-04-09 12:31:26 +0200

    release: 3.0.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ESClient.MixProject do
   def project do
     [
       app: :es_client,
-      version: "2.0.0",
+      version: "3.0.0",
       elixir: "~> 1.18",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
